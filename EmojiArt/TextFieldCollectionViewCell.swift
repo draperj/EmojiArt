1147fbc6569de57561db14886c8a45da4bc33c0a
Joshua Draper <draperj@nyc-p011356-m.local>
2018-04-11 16:55:49 -0400

    Changes for lecture 13

---
+++ EmojiArt/TextFieldCollectionViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  TextFieldCollectionViewCell.swift
+//  EmojiArt
+//
+//  Created by Joshua Draper on 4/11/18.
+//  Copyright © 2018 Joshua Draper. All rights reserved.
+//
+
+import UIKit
+
+class TextFieldCollectionViewCell: UICollectionViewCell, UITextFieldDelegate {
+    
+    @IBOutlet weak var textField: UITextField! {
+        didSet {
+            textField.delegate = self
+        }
+    }
+    
+    var resignationHandler: (() -> Void)?
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        resignationHandler?()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
